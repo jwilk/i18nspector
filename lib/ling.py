275457fbfb026f2732b9be22a2804d006a00cfeb
Jakub Wilk <jwilk@jwilk.net>
2012-10-22 22:20:32 +0200

    private/update-gettext-data: add a script to keep our language data in sync with gettext.

---
+++ lib/ling.py
@@ -0,0 +1,46 @@
+# Copyright © 2012 Jakub Wilk <jwilk@jwilk.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import configparser
+import os
+
+class LingData(object):
+
+    def __init__(self, datadir):
+        path = os.path.join(datadir, 'languages')
+        cp = configparser.RawConfigParser()
+        cp.read(path, encoding='UTF-8')
+        self._path = path
+        self._cp = cp
+
+    def get_primary_languages(self):
+        return list(self._cp)
+
+    def get_plural_forms(self, language):
+        return self._cp[language]['plural-forms']
+
+    def _set_plural_forms(self, language, s):
+        self._cp[language]['plural-forms'] = s
+
+    def _save(self):
+        with open(self._path, 'wt', encoding='UTF-8') as file:
+            self._cp.write(file)
+
+# vim:ts=4 sw=4 et
