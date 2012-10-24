a01b9ac6dc0011c11c16f25b9086f4ace3f90834
Jakub Wilk <jwilk@jwilk.net>
2012-10-24 22:18:57 +0200

    Check for encoding name validity and portability.

--- lib/encodings.py
+++ lib/encodings.py
@@ -18,6 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import codecs
 import configparser
 import encodings.aliases
 import os
@@ -28,24 +29,39 @@ class EncodingInfo(object):
         path = os.path.join(datadir, 'encodings')
         cp = configparser.ConfigParser(interpolation=None, default_section='')
         cp.read(path, encoding='UTF-8')
-        gpe = self.gettext_portable_encodings = set()
-        ppe = self.python_portable_encodings = set()
+        self._portable_encodings = penc = {}
         for encoding, extra in cp['portable-encodings'].items():
+            penc[encoding] = False
             if extra == '':
                 ''.encode(encoding)
-                ppe.add(encoding)
+                penc[encoding] = True
             elif extra == 'not-python':
                 pass
             else:
                 raise ValueError
-            gpe.add(encoding)
-        assert ppe.issubset(gpe)
-        self.python_portable_encodings = set(ppe)
-        self.gettext_portable_encodings = set(gpe)
         self._extra_aliases = dict(
             (key.lower().replace('-', '_'), value.lower().replace('-', '_'))
             for key, value in cp['extra-aliases'].items()
         )
+
+    def is_portable_encoding(self, encoding, python=True):
+        encoding = encoding.lower()
+        if encoding.startswith('iso_'):
+            encoding = 'iso-' + encoding[4:]
+        if python:
+            return self._portable_encodings.get(encoding, False)
+        else:
+            return encoding in self._portable_encodings
+
+    def propose_portable_encoding(self, encoding, python=True):
+        try:
+            new_encoding = codecs.lookup(encoding).name
+        except LookupError:
+            return
+        if new_encoding.startswith('iso8859'):
+            new_encoding = 'iso-' + new_encoding[3:]
+        if self.is_portable_encoding(new_encoding, python=python):
+            return new_encoding
 
     def install_extra_aliases(self):
         for key, value in self._extra_aliases.items():
