c606a92688a51f5720fe3606a99554e713ebb1e6
Jakub Wilk <jwilk@jwilk.net>
2013-06-04 15:49:20 +0200

    WIP

---
+++ doc/conf.py
@@ -0,0 +1,17 @@
+import sys, os
+
+needs_sphinx = '1.0'
+
+source_suffix = '.txt'
+
+master_doc = 'i18nspector'
+
+project = 'i18nspector'
+version = project + ' 0.19'
+
+today_fmt = '%Y-%m-%d'
+
+man_pages = [
+    ('i18nspector', 'i18nspector', u'checking tool for gettext POT, PO and MO files',
+     [], 1)
+]
