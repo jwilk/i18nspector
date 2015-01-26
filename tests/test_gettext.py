1d671f43497e1f94a7b95540f06de4423ba4cef4
Jakub Wilk <jwilk@jwilk.net>
2015-01-26 14:52:28 +0100

    lib.gettext: forbid * in the plural expression parser.

--- tests/test_gettext.py
+++ tests/test_gettext.py
@@ -265,6 +265,10 @@ class test_plural_exp:
         with assert_raises(self.error):
             self.t('6 >> 7')
 
+    def test_pow(self):
+        with assert_raises(self.error):
+            self.t('6 ** 7')
+
     def test_exotic_whitespace(self):
         with assert_raises(self.error):
             self.t('6 *\xA07')
