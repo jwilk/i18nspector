5dfc1a170727aa99037604ab8b8f3aaf4812bbf3
Jakub Wilk <jwilk@jwilk.net>
2016-02-16 16:27:30 +0100

    lib.strformat.python: add docstring.

--- lib/strformat/python.py
+++ lib/strformat/python.py
@@ -18,6 +18,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+'''
+string format checks: Python's %-formatting
+'''
+
 import collections
 import io
 
