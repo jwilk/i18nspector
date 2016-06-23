50ca5dd426f016ae7afe9965575f9c094c4a7aa9
Jakub Wilk <jwilk@jwilk.net>
2016-06-23 12:34:49 +0200

    tests/blackbox_tests: load tests only for wanted files.

--- tests/blackbox_tests/__init__.py
+++ tests/blackbox_tests/__init__.py
@@ -155,7 +155,8 @@ class Plugin(nose.plugins.Plugin):
                 return True
 
     def loadTestsFromFile(self, path):
-        yield TestCase(path)
+        if self.wantFile(path):
+            yield TestCase(path)
 
     def wantFunction(self, func):
         if getattr(func, 'redundant', False):
