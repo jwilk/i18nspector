d10a9cd02c55facf24dd20e47587e807a4b470b9
Jakub Wilk <jwilk@jwilk.net>
2016-07-08 21:51:23 +0200

    lib.cli: clarify --version description.

--- lib/cli.py
+++ lib/cli.py
@@ -152,7 +152,15 @@ def parse_jobs(s):
     return n
 parse_jobs.__name__ = 'jobs'
 
-class VersionAction(argparse._VersionAction):
+class VersionAction(argparse.Action):
+
+    def __init__(self, option_strings, dest=argparse.SUPPRESS):
+        super(VersionAction, self).__init__(
+            option_strings=option_strings,
+            dest=dest,
+            nargs=0,
+            help="show program's version information and exit"
+        )
 
     def __call__(self, parser, namespace, values, option_string=None):
         print('{prog} {0}'.format(__version__, prog=parser.prog))
