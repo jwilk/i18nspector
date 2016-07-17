115f493a5110425183ec38d0e2cccecdfa3f6a95
Jakub Wilk <jwilk@jwilk.net>
2016-07-17 16:26:52 +0200

    lib.check.msgrepr: add docstring.

--- lib/check/msgrepr.py
+++ lib/check/msgrepr.py
@@ -18,6 +18,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+'''
+string representation of messages
+'''
+
 from lib import tags
 
 def message_repr(message, template='{}'):
