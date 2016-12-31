40915acdba901226046954f4a2e6906559c0efc0
Jakub Wilk <jwilk@jwilk.net>
2016-12-31 22:09:10 +0100

    tests: prevent accidental byte-compilation with Python 2.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,2 @@
-import sys
-
-if sys.version_info < (3, 0):
-    raise ImportError('Python >= 3 is required')
-
-# vim:ts=4 sts=4 sw=4 et
+# pylint: disable=pointless-statement
+exec  # Python >= 3 is required
