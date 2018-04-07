jwilk/i18nspector

commit d594544960e03691048ddbf6aadbc00a6fb6a921

Makefile: install manpage only if it exists.
The manual page doesn't exist in a pristine git checkout,
but it does exist in a release tarball.

Makefile
@@ -46,8 +46,12 @@ install:
 	# library + data:
 	( find lib data -type f ! -name '*.py[co]' ) \
 	| xargs -t -I {} $(INSTALL) -p -D -m644 {} $(DESTDIR)$(basedir)/{}
+ifeq "$(wildcard .git doc/$(exe).1)" ".git"
+	# run "make -C doc" to build the manpage
+else
 	# manual page:
 	$(INSTALL) -p -D -m644 doc/$(exe).1 $(DESTDIR)$(mandir)/man1/$(exe).1
+endif
 
 .PHONY: test
 test:

commit 7fedbcae526b3fa6be4b289856976f8857826cf3

Makefile: don't check for .git existence.
Some users might use a git snapshot that doesn't have the .git directory
(for example, an hg-git clone, or a tarball automatically generated by
GitHub).

Fixes: d594544960e03691048ddbf6aadbc00a6fb6a921

Makefile
@@ -46,7 +46,7 @@ install:
 	# library + data:
 	( find lib data -type f ! -name '*.py[co]' ) \
 	| xargs -t -I {} $(INSTALL) -p -D -m644 {} $(DESTDIR)$(basedir)/{}
-ifeq "$(wildcard .git doc/$(exe).1)" ".git"
+ifeq "$(wildcard doc/$(exe).1)" ""
 	# run "$(MAKE) -C doc" to build the manpage
 else
 	# manual page: