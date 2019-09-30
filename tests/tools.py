73234242d6201ae4aa1fae8d1226c34d6a53ab35
Jakub Wilk <jwilk@jwilk.net>
2019-09-30 09:00:00 +0200

    tests/tools: disable Pylint's consider-using-sys-exit.

--- tests/tools.py
+++ tests/tools.py
@@ -55,6 +55,8 @@ def fork_isolation(f):
     # sys.exit() can't be used here, because nose catches all exceptions,
     # including SystemExit
 
+    # pylint:disable=consider-using-sys-exit
+
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
         readfd, writefd = os.pipe()
@@ -93,6 +95,8 @@ def fork_isolation(f):
                 pass
             else:
                 raise RuntimeError('unexpected isolated process status {}'.format(status))
+
+    # pylint:enable=consider-using-sys-exit
 
     return wrapper
 
