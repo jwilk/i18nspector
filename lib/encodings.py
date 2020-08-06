17f0f9a464abb32cd326acfe0d632b6694723c9f
Jakub Wilk <jwilk@jwilk.net>
2020-08-06 10:01:47 +0200

    lib.encodings: del iter var.

--- lib/encodings.py
+++ lib/encodings.py
@@ -138,6 +138,7 @@ if sys.version_info >= (3, 9):
     for _key in set(_portable_encodings) | set(_extra_encodings):
         assert _key not in _unmangle_encoding
         _unmangle_encoding[_key.replace('-', '_')] = _key
+    del _key
 
 def _read_control_characters():
     path = os.path.join(paths.datadir, 'control-characters')
