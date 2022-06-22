0937d186001f03fb65d46dee0a454c83ae8f20c1
Jakub Wilk <jwilk@jwilk.net>
2022-06-22 22:41:55 +0200

    lib.cli: use uppercase for CLI metavars.

--- lib/cli.py
+++ lib/cli.py
@@ -202,13 +202,13 @@ def main():
     initialize_terminal()
     ap = argparse.ArgumentParser(description=__doc__)
     ap.add_argument('--version', action=VersionAction)
-    ap.add_argument('-l', '--language', metavar='<lang>', help='assume this language')
+    ap.add_argument('-l', '--language', metavar='LANG', help='assume this language')
     ap.add_argument('--unpack-deb', action='store_true', help='allow unpacking Debian packages')
-    ap.add_argument('-j', '--jobs', type=parse_jobs, metavar='<n>', default=None, help='use <n> processes')
-    ap.add_argument('--parallel', type=int, metavar='<n>', default=None, help=argparse.SUPPRESS)  # renamed as -j/--jobs in 0.25
-    ap.add_argument('--file-type', metavar='<file-type>', help=argparse.SUPPRESS)
+    ap.add_argument('-j', '--jobs', type=parse_jobs, metavar='N', default=None, help='use N processes')
+    ap.add_argument('--parallel', type=int, metavar='N', default=None, help=argparse.SUPPRESS)  # renamed as -j/--jobs in 0.25
+    ap.add_argument('--file-type', metavar='FILE-TYPE', help=argparse.SUPPRESS)
     ap.add_argument('--traceback', action='store_true', help=argparse.SUPPRESS)
-    ap.add_argument('files', metavar='<file>', nargs='+')
+    ap.add_argument('files', metavar='FILE', nargs='+')
     options = ap.parse_args()
     files = options.files
     del options.files
