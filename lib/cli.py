jwilk/i18nspector

commit 6758176645a5e8e65c5296fc71ed4399519bdb4e

lib.cli: use the "fork" multiprocessing start method.
In Python 3.14, multiprocessing switched to "forkserver" as the default
start method, but our code is not ready for that:

    $ i18nspector -j2 *.po
    Traceback (most recent call last):
      ...
    RuntimeError:
            An attempt has been made to start a new process before the
            current process has finished its bootstrapping phase.

            This probably means that you are not using fork to start your
            child processes and you have forgotten to use the proper idiom
            in the main module:

                if __name__ == '__main__':
                    freeze_support()
                    ...

            The "freeze_support()" line can be omitted if the program
            is not going to be frozen to produce an executable.

            To fix this issue, refer to the "Safe importing of main module"
            section in https://docs.python.org/3/library/multiprocessing.html

    Traceback (most recent call last):
      ...
    ConnectionResetError: [Errno 104] Connection reset by peer

Switch back to the "fork" method for now.

https://github.com/jwilk/i18nspector/issues/20

https://github.com/python/cpython/issues/84559

lib/cli.py
@@ -26,6 +26,7 @@ import argparse
 import concurrent.futures
 import functools
 import io
+import multiprocessing
 import os
 import subprocess as ipc
 import sys
@@ -131,7 +132,8 @@ def check_all(paths, *, options):
             check_file(path, options=options)
     else:
         Executor = concurrent.futures.ProcessPoolExecutor
-        with Executor(max_workers=options.jobs) as executor:
+        mp_context = multiprocessing.get_context('fork')
+        with Executor(max_workers=options.jobs, mp_context=mp_context) as executor:
             check_file_opt = functools.partial(check_file_s, options=options)
             for s in executor.map(check_file_opt, paths):
                 sys.stdout.write(s)

commit e60750006a0a0160bd135cb18437fca6670a909e

lib.cli: restrict using the "fork" multiprocessing start method.
If the default multiprocessing start method is not fork(2)-based,
use that instead of switching to "fork".

Fixes: commit 6758176645a5e8e65c5296fc71ed4399519bdb4e
       ("lib.cli: use the 'fork' multiprocessing start method.")

lib/cli.py
@@ -132,7 +132,9 @@ def check_all(paths, *, options):
             check_file(path, options=options)
     else:
         Executor = concurrent.futures.ProcessPoolExecutor
-        mp_context = multiprocessing.get_context('fork')
+        mp_context = multiprocessing.get_context()
+        if mp_context.get_start_method() == 'forkserver':
+            mp_context = multiprocessing.get_context('fork')
         with Executor(max_workers=options.jobs, mp_context=mp_context) as executor:
             check_file_opt = functools.partial(check_file_s, options=options)
             for s in executor.map(check_file_opt, paths):